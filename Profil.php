cd52fc0ee6014379f797ac99be0278cea2eb8897
Gelleriewe <pierre.ollivier.wendling@gmail.com>
2018-09-12 11:48:24 +0200

    /n

--- Profil.php
+++ Profil.php
@@ -14,6 +14,22 @@
     <body background="Images/Sea.jpg" class="background">
         <?php include 'include/nav.php'; ?>
         <?php
+            $Profilage=$db->prepare("SELECT Name, FirstName, Nick, Mail FROM identifiants WHERE Identifiant = '$identifiant'");
+            $Profilage->execute();
+            $profil = $Profilage->fetchALL();
+
+            $Name=htmlspecialchars($profil[0][0]);
+            $Firstname=htmlspecialchars($profil[0][1]);
+            $Nick=htmlspecialchars($profil[0][2]);
+            $Mail=htmlspecialchars($profil[0][3]);
+            echo "<h3> Nom </h3>";
+            echo "$Name";
+            echo "<h3> Prénom </h3>";
+            echo "$Firstname";
+            echo "<h3> Pseudo </h3>";
+            echo "$Nick";
+            echo "<h3> Mail </h3>";
+            echo "$Mail";
 
 
 
