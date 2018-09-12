c243629fa43b523ef3e6548b2135ee9005cb6fc6
Antoine Mzoyer <35558783+AntoineMazoyer@users.noreply.github.com>
2018-09-12 08:18:06 +0200

    script accueil

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <script type = "text/javascript"
+ src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js">
+</script>
         <title></title>
         <?php include 'include/head.php'; ?>
                 <script src="./javascript/index.js"></script>
