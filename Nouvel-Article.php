6398d58b728467ada630c76b50a5e2acffaddaed
mathxi <mathieu.janio@espi.fr>
2018-09-12 15:34:31 +0200

    Update de l'ajout d'article

--- Nouvel-Article.php
+++ Nouvel-Article.php
@@ -9,7 +9,7 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
         <?php include 'include/head.php'; ?> 
-       <!--  CSS général -->
+        <!--  CSS général -->
         <link rel="stylesheet" href="./CSS/Nouvel_Article.css">
         <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=ehldmpzi4jvwzppzu2k6sier69zp9qexylmbt4p4rnh3duil"></script>
         <script src="./javascript/Nouvel_Article.js"></script>
@@ -18,20 +18,29 @@ and open the template in the editor.
     <body>
         <?php include 'include/nav.php'; ?>
         <div class="">
-            <form  method="POST" action="./SQL-query/insert-article-complet.php" style="margin-top: 5vh;">
+            <form  method="POST" enctype="multipart/form-data" action="./SQL-query/insert-article-complet.php" style="margin-top: 5vh;">
                 <div class="row">
-                <div class="input-field col s6">
-                    <input id="input_text" type="text" data-length="30" name="titre">
-                    <label for="input_text">Titre</label>
-                </div>
-                    
-                <div class="input-field col s6">
-                    <input id="input_text" type="text" data-length="80" name="description">
-                    <label for="input_text">Description concis</label>
-                </div>
+                    <div class="file-field input-field col s3">
+                        <div class="btn">
+                            <span>File</span>
+                            <input required type="file" accept="image/gif, image/jpeg" name="photoArticle">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text">
+                        </div>
+                    </div>
+                    <div class="input-field col s6">
+                        <input required id="input_text" type="text" data-length="30" maxlength="30" name="titre">
+                        <label for="input_text">Titre</label>
+                    </div>
+
+                    <div class="input-field col s6">
+                        <input required id="input_text" type="text" data-length="200" maxlength="200" name="description">
+                        <label for="input_text">Description concis</label>
+                    </div>
                 </div>
                 <textarea name="content"></textarea>
-                <input type="submit" value="Submit">
+                <input type="submit" value="submit">
 
             </form>
 
--- SQL-query/insert-article-complet.php
+++ SQL-query/insert-article-complet.php
@@ -1,38 +1,77 @@
 <?php
+
 include_once '../config/config.php';
 $db = new PDO("mysql:host=" . Config::SERVERNAME . ";dbname=" . Config::DBNAME, Config::USER, Config::PASSWORD, array(PDO::ATTR_PERSISTENT => true, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-/*
-$name = 'test';
-$content = 'testcontent';
- * */
-$name = $_POST['titre'];
-$content = $_POST['content'];
-$description = $_POST['description'];
-$campus = 'testcampus';
- 
-
-$url_img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0_2nqm0H20gpO-Pf9BsBwuAYt3McWcb-6rFs37i244h71Lyrnkg';
-$Tag_id = '1';
-$ID_Creator = '1';
-
-
-$req = $db->prepare("INSERT INTO article (Article_Name, description, Contents, Campus, url_img_summary, Tag_Id, ID_Creator)
-                     VALUES (?, ?, ?, ?, ?, ?, ?)");
-$req->bindParam(1, $name);
-$req->bindParam(2, $description);
-$req->bindParam(3, $content);
-$req->bindParam(4, $campus);
-$req->bindParam(5, $url_img);
-$req->bindParam(6, $Tag_id);
-$req->bindParam(7, $ID_Creator);
 
 
 
+//UPLOAD DU FICHIER
+$currentDir = getcwd();
+$uploadDirectory = "/uploads/images-articles/";
 
-var_dump($req->execute());
+$errors = []; // Store all foreseen and unforseen errors here
 
-     
+$fileExtensions = ['jpeg', 'jpg', 'png']; // Get all the file extensions
 
+$fileName = $_FILES['photoArticle']['name'];
+$fileSize = $_FILES['photoArticle']['size'];
+$fileTmpName = $_FILES['photoArticle']['tmp_name'];
+$fileType = $_FILES['photoArticle']['type'];
+$fileExtension = strtolower(end(explode('.', $fileName)));
+
+$uploadPath = $currentDir . $uploadDirectory . basename($fileName);
+
+if (isset($_POST['submit'])) {
+
+    if (!in_array($fileExtension, $fileExtensions)) {
+        $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
+    }
+
+    if ($fileSize > 2000000) {
+        $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
+    }
+
+    if (empty($errors)) {
+        $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+        if ($didUpload) {
+            echo "The file " . basename($fileName) . " has been uploaded";
+            //UPLOAD LIENS img + ARTICLE
+
+            $name = $_POST['titre'];
+            $content = $_POST['content'];
+            $description = $_POST['description'];
+            $campus = 'testcampus';
+
+
+            $url_img = $uploadPath;
+            $Tag_id = '1';
+            $ID_Creator = '1';
+
+
+            $req = $db->prepare("INSERT INTO article (Article_Name, description, Contents, Campus, url_img_summary, Tag_Id, ID_Creator)
+                VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $req->bindParam(1, $name);
+            $req->bindParam(2, $description);
+            $req->bindParam(3, $content);
+            $req->bindParam(4, $campus);
+            $req->bindParam(5, $url_img);
+            $req->bindParam(6, $Tag_id);
+            $req->bindParam(7, $ID_Creator);
+
+
+
+
+            var_dump($req->execute());
+        } else {
+            echo "An error occurred somewhere. Try again or contact the admin";
+        }
+    } else {
+        foreach ($errors as $error) {
+            echo $error . "These are the errors" . "\n";
+        }
+    }
+}
 ?>
 
 
