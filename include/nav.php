2bf0581ddb6eada50ab4f71548bbc64d42ab590f
Gelleriewe <pierre.ollivier.wendling@gmail.com>
2018-09-13 09:37:03 +0200

    ed

--- include/nav.php
+++ include/nav.php
@@ -1,8 +1,6 @@
 <nav id="navbar">
     <div>
       <a href="index.php">
-        logo</a>
-
         <img style="display:none;" src="" alt="étudiants, campus, bons plans, avatanges étudiants">
       </a>
     </div>
@@ -12,29 +10,25 @@
             <li><a href="index.php">Accueil</a></li>
             <li><a href="Bons-plans.php">Bon plans</a></li>
             <li><a href="Nouvel-Article.php">Nouveau</a></li>
-
-            <?php
-
-
-            $db = new PDO("mysql:host=" . Config::SERVERNAME . ";dbname=" . Config::DBNAME, Config::USER, Config::PASSWORD);
-            @$identifiant=$_COOKIE['IdConnexion'];
-
-            $testcookie=$db->prepare("SELECT Identifiant FROM identifiants WHERE Identifiant = '$identifiant'");
-            $testcookie->execute();
-            $result = $testcookie->fetchALL();
-            $nb2 = count($result);
-            if ($nb2==0) {
-              echo "<li><a href='connexion.php'>Log In</a></li>";
-            }
-            else {
-              echo "<li><a href='Profil.php'>Mon Profil</a></li>";
-            }
-            ?>
-
-
-
-
         </ul>
          <i class="material-icons">menu</i>
+
+         <?php
+
+
+         $db = new PDO("mysql:host=" . Config::SERVERNAME . ";dbname=" . Config::DBNAME, Config::USER, Config::PASSWORD);
+         @$identifiant=$_COOKIE['IdConnexion'];
+
+         $testcookie=$db->prepare("SELECT Identifiant FROM identifiants WHERE Identifiant = '$identifiant'");
+         $testcookie->execute();
+         $result = $testcookie->fetchALL();
+         $nb2 = count($result);
+         if ($nb2==0) {
+           echo "<li><a href='connexion.php'>Log In</a></li>";
+         }
+         else {
+           echo "<li><a href='Profil.php'>Mon Profil</a></li>";
+         }
+         ?>
     </div>
 </nav>
