6bb7167f352b6ff511f86cac18730395f3b0d7c6
mathxi <mathieu.janio@espi.fr>
2018-09-13 09:53:48 +0200

    Revert "kk"
    This reverts commit d0425833ed621931c5cbd972ed520821eb8c5ed4.

--- include/head.php
+++ include/head.php
@@ -13,8 +13,10 @@
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 <?php
-
+<<<<<<< HEAD
 include_once 'config/Config.php';
  ?>
-
-
+=======
+include './config/Config.php';
+?>
+>>>>>>> 667f6305683197c48d99bf32b5b417eae7f7ce1c
